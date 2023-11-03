45b0b9f488602fa1ba6152be8f6ffb1c47abed58
phuongtq <tranphuong.080@gmail.com>
2023-11-03 11:41:31 +0700

    test(upgrade-test-package): use build from local source

--- tests/integration_tests/configs/upgrade-test-package.nix
+++ tests/integration_tests/configs/upgrade-test-package.nix
@@ -1,12 +1,17 @@
 let
   pkgs = import ../../../nix { };
   fetchEthermint = rev: builtins.fetchTarball "https://github.com/evmos/ethermint/archive/${rev}.tar.gz";
-  released = pkgs.buildGo118Module rec {
-    name = "ethermintd";
-    src = fetchEthermint "d29cdad6e667f6089dfecbedd36bb8d3a2a7d025";
+  released = pkgs.buildGoApplication rec {
+    pname = "ethermintd";
+    version = "v0.22.0-t.1";
+    # src = fetchEthermint "d29cdad6e667f6089dfecbedd36bb8d3a2a7d025";
+    src = ../../../.;
+    pwd = src;
+    # modules = gomod2nix.toml;
     subPackages = [ "cmd/ethermintd" ];
-    vendorSha256 = "sha256-cQAol54b6hNzsA4Q3MP9mTqFWM1MvR5uMPrYpaoj3SY=";
+    # vendorSha256 = "sha256-cQAol54b6hNzsA4Q3MP9mTqFWM1MvR5uMPrYpaoj3SY=";
     doCheck = false;
+    CGO_ENABLED = "1";
   };
   current = pkgs.callPackage ../../../. { };
 in
